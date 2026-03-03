c2311b7a070175b4fb3ab62c5a4fe6a96ba5de08
Sistemas-OroJuez <sistemas.orj@gmail.com>
2026-03-03 14:26:28 -0500

    localidades3

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,6 @@
-﻿import { createClient } from '@supabase/supabase-js';
+﻿import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://tzllavurccjvhzrehyug.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6bGxhdnVyY2Nqdmh6cmVoeXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMDAzNTQsImV4cCI6MjA4Nzc3NjM1NH0.4QwByPX7EgGJewQ5Wao7Y2vothMzMjFUvRyFdS36nt4'
 
-// Validamos que existan las variables antes de crear el cliente
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("⚠️ Error: Variables de Supabase no detectadas.");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
