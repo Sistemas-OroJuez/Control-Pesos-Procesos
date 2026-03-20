b851acff3ed6c31ef53307b4dc5e35e1dd4584e4
Sistemas-OroJuez <sistemas.orj@gmail.com>
2026-03-20 16:01:39 -0500

    ocr

--- src/app/api/ocr/route.ts
+++ src/app/api/ocr/route.ts
@@ -4,7 +4,7 @@ import { ImageAnnotatorClient } from '@google-cloud/vision';
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
-  const VERSION = "REINTENTO_FINAL_V7"; 
+  const VERSION = "REINTENTO_FINAL_V8"; 
 
   try {
     const { fotoUrl } = await request.json();
